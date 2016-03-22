1f58a1712481db4437e6dd8b4c3fb3fec7ef522e
k-may <kevmayo@gmail.com>
2016-03-22 17:16:20 +0100

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,21 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var notify = require('gulp-notify');
 
+var src = [
+	"src/SVGMorphSequencer.js",
+	"src/SVGMorphPath.js",
+	"src/SVGMorphDrawer.js"
+];
+
 gulp.task('js-min', function () {
-	return gulp.src('src/**/*.js')
+	return gulp.src(src)//'src/**/*.js')
 		.pipe(uglify())
 		.pipe(concat('SVGMorph.min.js'))
 		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('js', function () {
-	return gulp.src('src/**/*.js')
+	return gulp.src(src)//'src/**/*.js')
 		.pipe(concat('SVGMorph.js'))
 		.pipe(gulp.dest('dist'));
 });
