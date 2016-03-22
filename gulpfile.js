563278a3bf9bff62a7b761088b96cd520cf5b8a2
k-may <kevmayo@gmail.com>
2016-03-22 16:46:00 +0100

    dist

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,23 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var notify = require('gulp-notify');
 
-gulp.task('js', function () {
+gulp.task('js-min', function () {
 	return gulp.src('src/**/*.js')
 		.pipe(uglify())
 		.pipe(concat('SVGMorph.min.js'))
-		.pipe(gulp.dest('dist'))
-		.pipe(notify({ message: 'Finished minifying JavaScript'}));
+		.pipe(gulp.dest('dist'));
+});
+
+gulp.task('js', function () {
+	return gulp.src('src/**/*.js')
+		.pipe(concat('SVGMorph.js'))
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', function () {
-	gulp.run('js');
+	gulp.run(['js', 'js-min']);
 
 	gulp.watch('*/*.js', function () {
-		gulp.run('js');
+		gulp.run(['js', 'js-min']);
 	});
 });
